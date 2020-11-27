f98baf2edb51cfa829de702b4e244d8eb6a8cd0b
Michael Iodice <miodice3@gmail.com>
2020-11-27 11:29:21 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :students, only: :index
-  get '/students/:id', to: 'students#show', as: 'student'
+  resources :students, only: [:index, :new, :create, :show]
+  # get '/students/new', to: 'students#new'
+  # get '/students/:id', to: 'students#show', as: 'student'
+  # get is a method that takes in 2 arguments, first being a string, second is a hash. 
+  #get('/students/:id', {to: 'students#show'}) rails just interprets this w/o paren / braces
 end
